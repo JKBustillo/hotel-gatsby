a6fdb601c38061d799627fa8da13b5cf0c441e8a
Jose Bustillo <josebustillodealba@gmail.com>
2020-06-27 22:16:24 -0500

    feat: text over the image

--- src/components/hotelImage.js
+++ src/components/hotelImage.js
@@ -7,6 +7,34 @@ const ImageBackground = styled(BackgroundImage)`
     height: 700px;
 `;
 
+const ImageText = styled.div`
+    background-image: linear-gradient(to top, rgba(34, 49, 63, .8), rgba(34, 49, 63, .8));
+    color: white;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+    align-items: center;
+    justify-content: center;
+
+    h1 {
+        font-size: 4rem;
+        margin: 0;
+
+        @media (min-width: 992px) {
+            font-size: 6rem;
+        }
+    }
+
+    p {
+        font-size: 2rem;
+
+        @media (min-width: 992px) {
+            font-size: 2.6rem;
+        }
+    }
+`;
+
 const HotelImage = () => {
     const { image } = useStaticQuery(graphql`
         query {
@@ -24,7 +52,10 @@ const HotelImage = () => {
 
     return (
         <ImageBackground tag="section" fluid={image.sharp.fluid} fadeIn="soft">
-
+            <ImageText>
+                <h1>Welcome to Hotel Gatsby</h1>
+                <p>The best hotel for enjoying your vacations</p>
+            </ImageText>
         </ImageBackground>
     );
 }
