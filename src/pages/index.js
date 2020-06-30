86429bed8e99e47bbd6a297f7586e9988b042411
Jose Bustillo <josebustillodealba@gmail.com>
2020-06-30 14:17:29 -0500

    feat: styles to make it look like a card

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { css } from "@emotion/core";
+import styled from '@emotion/styled';
 
 import Layout from "../components/layout";
 import HotelImage from "../components/hotelImage";
@@ -7,9 +8,21 @@ import HomeContent from "../components/homeContent";
 import useRooms from "../hooks/useRooms";
 import RoomPreview from "../components/roomPreview";
 
+const RoomList = styled.ul`
+  max-width: 1200px;
+  width: 95%;
+  margin: 4rem auto 0 auto;
+
+  @media (min-width: 768px) {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    column-gap: 3rem;
+  }
+`;
+
 const IndexPage = () => {
   const rooms = useRooms();
-  
+
   return (
     <Layout>
       <HotelImage />
@@ -24,14 +37,14 @@ const IndexPage = () => {
         `}
       >Our rooms</h2>
 
-      <ul>
+      <RoomList>
         {rooms.map(room => (
           <RoomPreview
             key={room.id}
             room={room}
           />
         ))}
-      </ul>
+      </RoomList>
     </Layout>
   )
 }
