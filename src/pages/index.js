0f1b91cf10f46fd54e7a3cf1f7516ee94d47d32e
Jose Bustillo <josebustillodealba@gmail.com>
2020-06-30 11:59:18 -0500

    feat: show all the rooms in the home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,39 @@
 import React from "react";
+import { css } from "@emotion/core";
+
 import Layout from "../components/layout";
 import HotelImage from "../components/hotelImage";
 import HomeContent from "../components/homeContent";
+import useRooms from "../hooks/useRooms";
+import RoomPreview from "../components/roomPreview";
 
-const IndexPage = () => (
-  <Layout>
-    <HotelImage />
+const IndexPage = () => {
+  const rooms = useRooms();
+  
+  return (
+    <Layout>
+      <HotelImage />
+  
+      <HomeContent />
 
-    <HomeContent />
-  </Layout>
-)
+      <h2
+        css={css`
+          text-align: center;
+          margin-top: 5rem;
+          font-size: 3rem;
+        `}
+      >Our rooms</h2>
+
+      <ul>
+        {rooms.map(room => (
+          <RoomPreview
+            key={room.id}
+            room={room}
+          />
+        ))}
+      </ul>
+    </Layout>
+  )
+}
 
 export default IndexPage;
