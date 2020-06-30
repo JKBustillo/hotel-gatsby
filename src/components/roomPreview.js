6f14f115f84c4393e9d74da6bf86b417ec54c84d
Jose Bustillo <josebustillodealba@gmail.com>
2020-06-30 11:59:52 -0500

    feat: display all the roms

---
+++ src/components/roomPreview.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Image from 'gatsby-image';
+
+const RoomPreview = ({ room }) => {
+    const { title, content, image, slug } = room;
+
+    return (
+        <div>
+            <Image
+                fluid={image.fluid}
+            />
+            <div>
+                <h3>{title}</h3>
+                <p>{content}</p>
+            </div>
+        </div>
+    );
+}
+ 
+export default RoomPreview;
