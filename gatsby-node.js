1f4e1572d367c110dcc844805e8cec6982e93761
Jose Bustillo <josebustillodealba@gmail.com>
2020-07-01 16:50:18 -0500

    feat!: conf to generate the pages of the rooms according to the slug of each one

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,28 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+exports.createPages = async ({ actions, graphql, reporter }) => {
+    const response = await graphql(`
+        query {
+            allDatoCmsRoom {
+                nodes {
+                    slug
+                }
+            }
+        }
+    `);
 
-// You can delete this file if you're not using it
+    if (response.errors) {
+        reporter.panic('No results: ', response.errors);
+    }
+
+    // IF there exists rooms
+    const rooms = response.data.allDatoCmsRoom.nodes;
+
+    rooms.forEach(room => {
+        actions.createPage({
+            path: room.slug,
+            component: require.resolve('./src/components/rooms.js'),
+            context: {
+                slug: room.slug
+            }
+        });
+    });
+}
